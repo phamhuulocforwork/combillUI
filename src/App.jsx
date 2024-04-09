e519c0d2549c63f4cd7437cdfe1c89aef9303852
HuuLoc <phamhuulocforwork@gmail.com>
2024-04-09 19:55:37 +0700

    Team page

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,7 @@
-import ApiTokenForm from './features/ApiTokenForm'
-import Calendar from './features/Calendar'
-import CollectionList from './features/CollectionList'
-import DatePicker from './features/DatePicker'
-import Dropdown from './features/Dropdown'
-import DropdownMenu from './features/DropdownMenu'
-import Feedback from './features/Feedback'
-import JobCard from './features/JobCard'
-import List from './features/List'
-import MessageBox from './features/MessageBox'
-import PopupNotifications from './features/PopupNotifications'
-import ProductReview from './features/ProductReviewModal'
-import SlideInNotifications from './features/SlideInNotifications'
-import Subscription from './features/Subscription'
-import TextFormattingToolbar from './features/TextFormattingToolbar'
-import Todo from './features/Todo'
-import UserList from './features/User list'
-import UserProfile from './features/UserProfile'
-import VideoPlayer from './features/VideoPlayer'
 import TeamPage from './views/TeamPage'
 
 function App() {
-  return (
-    <div className="flex w-full items-center justify-center bg-blue-300 min-h-screen h-fit">
-      {/* <Todo></Todo> */}
-      {/* <UserProfile></UserProfile> */}
-      {/* <List></List> */}
-      {/* <JobCard></JobCard> */}
-      {/* <MessageBox></MessageBox> */}
-      {/* <CollectionList></CollectionList> */}
-      {/* <ProductReview></ProductReview> */}
-      {/* <Subscription></Subscription> */}
-      {/* <UserList></UserList> */}
-      {/* <Feedback></Feedback> */}
-      {/* <Calendar></Calendar> */}
-      {/* <DropdownMenu></DropdownMenu> */}
-      {/* <DatePicker></DatePicker> */}
-      {/* <SlideInNotifications></SlideInNotifications> */}
-      {/* <Dropdown></Dropdown> */}
-      {/* <ApiTokenForm></ApiTokenForm> */}
-      {/* <TextFormattingToolbar></TextFormattingToolbar> */}
-      {/* <VideoPlayer></VideoPlayer> */}
-      {/* <SimpleMenu></SimpleMenu> */}
-      <TeamPage></TeamPage>
-    </div>
-  )
+  return <TeamPage></TeamPage>
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/*{svg}'],
   theme: {
     extend: {
       backgroundImage: {
-        'logo': "url('/public/huuloclogo.svg')",
+        'grid-pattern': "url('/public/gridpattern.svg')",
       },
     },
   },
