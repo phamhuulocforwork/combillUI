1adfb25c795d8b20e44ebfbcc070ae1fa06ccd36
HuuLoc <phamhuulocforwork@gmail.com>
2024-04-12 22:59:23 +0700

    FlashMessage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import PricingPage from './views/PricingPage'
+import FlashMessage from './features/FlashMessage'
 
 function App() {
-  return <PricingPage></PricingPage>
+  return (
+    <div className="flex items-center justify-center bg-blue-300 w-full h-screen gap-4">
+      <FlashMessage state={'success'}></FlashMessage>
+      <FlashMessage state={'error'}></FlashMessage>
+    </div>
+  )
 }
 
 export default App
