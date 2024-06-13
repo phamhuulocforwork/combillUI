cea45a757c86d6eb64b693c307a14aee5f7c16cc
HuuLoc <phamhuulocforwork@gmail.com>
2024-06-13 17:02:22 +0700

    Restructure directories

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyle } from './components/index.component'
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <GlobalStyle>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </GlobalStyle>
+  </BrowserRouter>,
 )
 
 // If you want to start measuring performance in your app, pass a function
