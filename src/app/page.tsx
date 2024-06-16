8420ad6851df1f2849a1577edfe6764131dc22ec
HuuLoc <phamhuulocforwork@gmail.com>
2024-06-16 13:47:17 +0700

    Reset the project with React, Typescript, TailwindCSS, StoryBook

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/Button";
+
+export default function Home() {
+  return (
+    <main className="flex w-screen h-screen justify-center items-center bg-blue-200">
+      <Button></Button>
+    </main>
+  );
+}
