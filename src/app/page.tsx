979aa900ce8d8e24f65ea95976108c02f3750946
HuuLoc <phamhuulocforwork@gmail.com>
2024-06-17 23:28:02 +0700

    Avatar, checkbox, label, textarea, select, separator, skeleton, slider, tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,7 @@
-import { Button } from "@/components/Button";
-import { Move } from "lucide-react";
+import { cn } from '@/lib/utils';
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-4 w-screen h-screen justify-center items-center bg-white">
-      <Button variant="default" size="xs">
-        Default Button
-      </Button>
-      <Button variant="block">Block Button</Button>
-      <Button variant="outline" size="sm">
-        Outline Button
-      </Button>
-      <Button variant="destructive" size="lg">
-        Destructive Button
-      </Button>
-      <Button variant="secondary">Secondary Button</Button>
-      <Button variant="link">Link</Button>
-      <Button variant="outline" size="icon">
-        <Move />
-      </Button>
-    </main>
+    <main className='bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark flex h-screen w-screen flex-col items-center justify-center gap-4'></main>
   );
 }
---
+++ src/components/ui/ResponsiveTextarea.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import * as React from 'react';
+import { cn } from '@/lib/utils';
+
+export interface ResponsiveTextareaProps
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const ResponsiveTextarea = React.forwardRef<
+  HTMLTextAreaElement,
+  ResponsiveTextareaProps
+>(({ className, ...props }, ref) => {
+  const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+  const [val, setVal] = React.useState<string>('');
+
+  React.useEffect(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.style.height = 'auto';
+      textAreaRef.current.style.height =
+        textAreaRef.current.scrollHeight + 'px';
+    }
+  }, [val]);
+
+  return (
+    <textarea
+      className={cn(
+        'border-input bg-background dark:bg-background-dark ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex min-h-[80px] w-full resize-none overflow-hidden rounded-md border px-3 py-2 text-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        className,
+      )}
+      ref={textAreaRef}
+      onChange={(e) => setVal(e.target.value)}
+      {...props}
+    />
+  );
+});
+ResponsiveTextarea.displayName = 'Textarea';
+
+export { ResponsiveTextarea as ResponsiveTextarea };
