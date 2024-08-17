aaa09badab7d0ab95aab93411265866f7804598e
HuuLoc <phamhuulocforwork@gmail.com>
2024-08-18 00:36:12 +0700

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/Button';
 
 export default function Home() {
   return (
-    <main className='bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark flex h-screen w-screen flex-col items-center justify-center gap-4'></main>
+    <main className='flex min-h-screen w-full items-center justify-center'>
+      <div className='flex flex-col items-center justify-center gap-4'>
+        <h1 className='text-center text-4xl font-bold'>
+          Welcome to the TailwindCSS UI Kit!
+        </h1>
+        <p className='text-center text-lg'>
+          This is a Next.js starter template with TailwindCSS and TypeScript.
+        </p>
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,22 +9,18 @@ const config = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
-  prefix: '',
   theme: {
-    container: {
-      center: true,
-      padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
-    },
     extend: {
+      backgroundImage: {
+        light: "url('/noise-light.gif')",
+        dark: "url('/noise-dark.gif')",
+      },
       colors: {
-        background: {
-          DEFAULT: '#ffffff',
-          light: '#ffffff',
-          dark: '#0b0b0b',
-        },
+        // background: {
+        //   DEFAULT: '',
+        //   light: '',
+        //   dark: '',
+        // },
         text: {
           DEFAULT: '#0f172a',
           light: '#0f172a',
@@ -113,20 +109,20 @@ const config = {
         //   foreground: "hsl(var(--card-foreground))",
         // },
       },
-      keyframes: {
-        'accordion-down': {
-          from: { height: '0' },
-          to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-          from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: '0' },
-        },
-      },
-      animation: {
-        'accordion-down': 'accordion-down 0.2s ease-out',
-        'accordion-up': 'accordion-up 0.2s ease-out',
-      },
+      // keyframes: {
+      //   'accordion-down': {
+      //     from: { height: '0' },
+      //     to: { height: 'var(--radix-accordion-content-height)' },
+      //   },
+      //   'accordion-up': {
+      //     from: { height: 'var(--radix-accordion-content-height)' },
+      //     to: { height: '0' },
+      //   },
+      // },
+      // animation: {
+      //   'accordion-down': 'accordion-down 0.2s ease-out',
+      // 'accordion-up': 'accordion-up 0.2s ease-out',
+      // },
     },
   },
   plugins: [require('tailwindcss-animate')],
