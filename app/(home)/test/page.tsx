dd61bad4705457975280eb3ca8abda521a0abd49
Pham Huu Loc <phamhuulocforwork@gmail.com>
2025-04-07 23:00:54 +0700

    refactor:  restructure test page, update theme

--- app/(home)/test/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function TestPage() {
-  return (
-    <section className='container flex flex-col items-center justify-center gap-4'>
-      <h1>This is a test page</h1>
-    </section>
-  );
-}
---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <main className='flex flex-col items-center justify-center gap-4 min-h-screen bg-red-500'>
+      <section className='container flex flex-col items-center justify-center gap-4'>
+        <h1>This is a test page</h1>
+      </section>
+    </main>
+  );
+}
